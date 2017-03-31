cc404b055b30a47d3eda068f9e49c39294f51426
админ <admin@mac-mini-admin.local>
2017-03-30 22:26:11 -0700

    Initial Commit

---
+++ TechMasTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechMasTest
+//
+//  Created by админ on 30.03.17.
+//  Copyright © 2017 админ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
